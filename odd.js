c83dc916cb11fef27e482c13b67f538c819fcd8f
Sakshi Shetty <sakshishetty399@gmail.com>
2023-06-08 12:18:56 -0500

    Checking whether number is odd

---
+++ odd.js
@@ -0,0 +1,9 @@
+const isOdd = function (num) {
+  return num % 2 !== 0
+}
+console.log("9 is odd: " + isOdd(9));
+console.log("4 is odd: " + isOdd(4));
+console.log("15 is odd: " + isOdd(15));
+console.log("78 is odd: " + isOdd(78));
+
+
